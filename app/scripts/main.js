11f552089bc5089da267937846824d46e0e822fc
Nathan <nathancookster@gmail.com>
2014-04-11 01:47:53 -0500

    Convert space indentation to tabs.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,37 +2,37 @@
 'use strict';
 
 require.config({
-    shim: {
-        underscore: {
-            exports: '_'
-        },
-        backbone: {
-            deps: [
-                'underscore',
-                'jquery'
-            ],
-            exports: 'Backbone'
-        },
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
-        }
-    },
-    paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/underscore/underscore',
-        bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
-        card: 'models/card',
-        deck: 'collections/deck'
-    }
+	shim: {
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: [
+				'underscore',
+				'jquery'
+			],
+			exports: 'Backbone'
+		},
+		bootstrap: {
+			deps: ['jquery'],
+			exports: 'jquery'
+		}
+	},
+	paths: {
+		jquery: '../bower_components/jquery/dist/jquery',
+		backbone: '../bower_components/backbone/backbone',
+		underscore: '../bower_components/underscore/underscore',
+		bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
+		card: 'models/card',
+		deck: 'collections/deck'
+	}
 });
 
 require([
-    'backbone',
-    'card',
-    'deck'
+	'backbone',
+	'card',
+	'deck'
 ], function (Backbone, Card, Deck) {
-    Backbone.history.start();
-    window.deck = new Deck();
+	Backbone.history.start();
+	window.deck = new Deck();
 });
